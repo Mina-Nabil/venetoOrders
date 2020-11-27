a359800cd0f54b1afc737260cb56913a62ab64b2
Mina Nabil <mina9492@hotmail.com>
2020-11-27 12:52:30 +0200

    Adding Address

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -69,9 +69,9 @@ class ReportsController extends Controller
         $start = new DateTime($request->from);
         $end = new DateTime($request->to);
         if ($request->detailed)
-            $data['items'] = Order::getInventoryReport($start, $end, $request->type);
-        else
             $data['items'] = Order::getDetailedInventoryReport($start, $end, $request->type);
+        else
+            $data['items'] = Order::getInventoryReport($start, $end, $request->type);
         $data['start'] = $start;
         $data['end']    = $end;
         $data['detailed'] = $request->detailed ?? false;
