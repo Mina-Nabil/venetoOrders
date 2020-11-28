b3b860fcc9557ecb30ed5fecb98830153fb7c660
Mina Nabil <mina9492@hotmail.com>
2020-11-28 17:21:47 +0200

    Adding Address

--- resources/views/reports/inventory.blade.php
+++ resources/views/reports/inventory.blade.php
@@ -36,14 +36,14 @@
                             <td>{{$row->BRND_NAME}}</td>
                             <td>{{$row->MODL_NAME}}-{{$row->MODL_UNID}}</td>
                             @if($detailed)
-                            <td>{{$row->total36}}</td>
-                            <td>{{$row->total38}}</td>
-                            <td>{{$row->total40}}</td>
-                            <td>{{$row->total42}}</td>
-                            <td>{{$row->total44}}</td>
-                            <td>{{$row->total46}}</td>
-                            <td>{{$row->total48}}</td>
-                            <td>{{$row->total50}}</td>
+                            <td>{{$row->total36 ?? 0}}</td>
+                            <td>{{$row->total38 ?? 0}}</td>
+                            <td>{{$row->total40 ?? 0}}</td>
+                            <td>{{$row->total42 ?? 0}}</td>
+                            <td>{{$row->total44 ?? 0}}</td>
+                            <td>{{$row->total46 ?? 0}}</td>
+                            <td>{{$row->total48 ?? 0}}</td>
+                            <td>{{$row->total50 ?? 0}}</td>
                             @endif
                             <td>{{$row->soldCount}}</td>
                             <td>{{number_format($row->averagePrice)}}</td>
